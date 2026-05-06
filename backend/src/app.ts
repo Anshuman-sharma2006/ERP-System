579fadf158f3d05cc553ea3e91fec77b843a1aef
Anshuman-sharma2006 <anshumansharma9451@gmail.com>
2026-05-07 00:35:55 +0530

    n

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,47 +1,17 @@
 import * as dotenv from "dotenv";
-dotenv.config(); // ✅ load env first
-
 import express from "express";
 import cors from "cors";
-import connectDB from "./configs/db.connect";
+import connectDB from "./configs/db.connect"
 import { mainRouter } from "./middleware/mainRouter";
-import { log } from "console";
 
 const app = express();
+connectDB()
+dotenv.config();
 
-// ✅ connect DB
-connectDB();
-
-// ✅ allowed origins (local + production)
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL
-];
-console.log(process.env.FRONTEND_URL)
-// ✅ CORS setup
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin as string)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-  })
-);
-
-// ✅ middleware
+const FRONTEND_UR = process.env.FRONTEND_URL || "http://localhost:5173";
+console.log("FRONTEND_URL:", FRONTEND_UR);
+app.use(cors({origin: FRONTEND_UR , methods: ["GET","POST"]}));
 app.use(express.json());
-
-// ✅ routes
-app.use("/", mainRouter);
-
-// ✅ test route (optional)
-app.get("/", (req, res) => {
-  res.send("Server is running ✅");
-});
+app.use('/', mainRouter);
 
 export { app };
